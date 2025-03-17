41efd6f5e9c4d1ba64005623c0e2728ca1dfbf48
jonasoft86 <jonathan.monroy@live.com>
2025-03-17 17:35:33 -0300

    Welcome Screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,84 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native'
 import React from 'react'
-import ColorList from '@/components/CodeList'
+import { router } from 'expo-router'
+import { Colors } from '@/constants/Colors'
+import Animated, { FadeInDown, FadeInRight } from 'react-native-reanimated'
+import { StatusBar } from "expo-status-bar"
 
-const Home = () => {
+const Page = () => {
   return (
-    <View>
-      <ColorList color="#0891b2" />
+    <View style={styles.container}>
+      <StatusBar style='light' />
+      <ImageBackground
+        source={require("@/assets/images/getting-started.jpg")}
+        style={{flex:1}}
+        resizeMode='cover'
+      >
+        <View style={styles.wrapper}>
+          <Animated.Text 
+            style={styles.title} 
+            entering={FadeInRight.delay(300).duration(500)}>
+              Stay Updated!
+          </Animated.Text>
+          <Animated.Text 
+            style={styles.description}
+            entering={FadeInRight.delay(700).duration(500)}>
+              Get breacking news
+            </Animated.Text>
+          <Animated.View entering={FadeInDown.delay(1200).duration(500)}>
+            <TouchableOpacity style={styles.btn} onPress={()=> router.replace("/(tabs)")}>
+              <Text style={styles.btnText}>Get Started</Text>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+      </ImageBackground>
+
     </View>
   )
 }
 
-export default Home
+export default Page
+
+
+const styles = StyleSheet.create({
+    container:{
+      flex: 1
+    },
+    wrapper: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      paddingBottom: 50,
+      paddingHorizontal: 30,
+      gap: 10,
+      backgroundColor: 'rgba(0,0,0,0.5)'
+    },
+    title: {
+      color: Colors.white,
+      fontSize: 24,
+      fontWeight: '600',
+      letterSpacing: 1.5,
+      lineHeight: 36,
+      textAlign: 'center'
+    },
+    description: {
+      color: Colors.white,
+      fontSize: 16,
+      fontWeight: '500',
+      letterSpacing: 1.2,
+      lineHeight: 22,
+      textAlign: 'center'
+    },
+    btn:{
+      backgroundColor: Colors.tint,
+      paddingVertical: 15,
+      marginVertical: 20,
+      alignItems: 'center',
+      borderRadius: 20
+    },
+    btnText:{
+      color: Colors.white,
+      fontSize: 16,
+      fontWeight: '700',
+    }
+
+})

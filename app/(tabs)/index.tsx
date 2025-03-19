520b31067e8b0bcfd488ffdd2dfae784caba0416
jonasoft86 <jonathan.monroy@live.com>
2025-03-19 13:32:37 -0300

    News List

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text, ActivityIndicator } from 'react-native'
+import { StyleSheet, ActivityIndicator, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import Header from '@/components/Header'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
@@ -7,14 +7,17 @@ import { NewsDataType } from '@/types'
 import axios from 'axios'
 import BreakingNews from '@/components/BreakingNews'
 import Categories from '@/components/Categories'
+import NewsList from '@/components/NewsList'
 
 const Home = () => {
   const { top : safeTop } = useSafeAreaInsets();
   const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([]);
+  const [news, setNews] = useState<NewsDataType[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getBreakingNews();
+    getNews();
   }, [])
   
   const getBreakingNews = async() => {
@@ -31,14 +34,32 @@ const Home = () => {
     }
   }
 
+  const getNews = async (category:string = '') => {
+    try {
+      let categoryString = '';
+      if(category.length !== 0){
+        categoryString = `&category=${category}`
+      }
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=cl&language=es&image=1&removeduplicate=1&size=10${categoryString}`;
+      const response = await axios .get(URL);
+
+      if(response && response.data){
+        setNews(response.data.results);
+        setIsLoading(false)
+      }
+    } catch (err: any){
+      console.log("Error Message: ", err.message)
+    }
+  }
+
   const onCatChange = (category: string) => {
     console.log('Category: ', category);
-    //setNews([]);
-    //getNews(category);
+    setNews([]);
+    getNews(category);
   }
 
   return (
-    <View style={[styles.container, {paddingTop: safeTop}]}>
+    <ScrollView style={[styles.container, {paddingTop: safeTop}]}>
       <Header />
       <SearchBar />
       {
@@ -49,7 +70,8 @@ const Home = () => {
         )
       }
       <Categories onCategoryChanged={onCatChange}/>
-    </View>
+      <NewsList newsList={news}/>
+    </ScrollView>
   )
 }
 
--- constants/Icons.tsx
+++ constants/Icons.tsx
@@ -3,26 +3,26 @@ import { Ionicons } from "@expo/vector-icons";
 export const icon = {
   index: ({ color, focused }: { color: string; focused: boolean }) =>
     focused ? (
-      <Ionicons name="home" size={9} color={color} />
+      <Ionicons name="home" size={5} color={color} />
     ) : (
-      <Ionicons name="home-outline" size={4} color={color} />
+      <Ionicons name="home-outline" size={2} color={color} />
     ),
   discover: ({ color, focused }: { color: string; focused: boolean }) =>
     focused ? (
-      <Ionicons name="compass" size={4} color={color} />
+      <Ionicons name="compass" size={5} color={color} />
     ) : (
-      <Ionicons name="compass-outline" size={4} color={color} />
+      <Ionicons name="compass-outline" size={2} color={color} />
     ),
   saved: ({ color, focused }: { color: string; focused: boolean }) =>
     focused ? (
-      <Ionicons name="bookmarks" size={4} color={color} />
+      <Ionicons name="bookmarks" size={5} color={color} />
     ) : (
-      <Ionicons name="bookmarks-outline" size={4} color={color} />
+      <Ionicons name="bookmarks-outline" size={2} color={color} />
     ),
   settings: ({ color, focused }: { color: string; focused: boolean }) =>
     focused ? (
-      <Ionicons name="settings" size={9} color={color} />
+      <Ionicons name="settings" size={5} color={color} />
     ) : (
-      <Ionicons name="settings-outline" size={6} color={color} />
+      <Ionicons name="settings-outline" size={2} color={color} />
     ),
 };
